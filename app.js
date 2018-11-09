6d3b72301e24e6774eb02b5cab365bbab30e85a3
gaoyanbin <gaoyanbin@yahoo.com>
2018-11-09 14:47:23 -0700

    test

---
+++ app.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', abcd())
+
+
+function testMe() {
+
+    if (aa==1){
+        if (bb==2){
+            if (cc==3){
+                
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function abcd() {
+    // test for peek
+    
+    
+    
+
+}
